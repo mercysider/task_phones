e25dbb1350a76a9c0f3022f897cbd1ae207989ec
mercysider <mercysider@mail.ru>
2023-04-22 01:17:46 +0300

    add delete and change fubctions

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def main_func():
             case '2':
                 operations.search_contact(menu.search())
             case '3':
+                a, b, c = menu.change()
+                operations.change_contact(a, b, c)
+            case '4':
+                operations.delete_contact(menu.search())
+            case '5':
                 operations.show_list()
             case _:
                 flag = False
--- operations.py
+++ operations.py
@@ -17,6 +17,44 @@ def search_contact(name):
         print(f'Всего найдено контактов: {count}')
     data.close()
 
+def delete_contact(name):
+    data = open('file.txt', 'r', encoding='utf-8')
+    new_list = list()
+    for line in data:
+        if name.upper() not in line.upper():
+            new_list.append(line)
+    data.close()
+
+    data = open('file.txt', 'w', encoding='utf-8')
+    for line in new_list:
+        data.writelines(line)
+    data.close()
+
+def change_contact(element, before, after):
+    data = open('file.txt', 'r', encoding='utf-8')
+    match element:
+            case '1':
+                i = 0
+                j = 15
+            case '2':
+                i = 16
+                j = 36
+            case '3':
+                i = 37
+                j = 49
+    new_list = list()
+    for line in data:
+        if before.upper() not in line[i:j].upper():
+            new_list.append(line)
+        else:
+            new_list.append(line[:i] + after.ljust(j-i) + line[j:])            
+    data.close()
+
+    data = open('file.txt', 'w', encoding='utf-8')
+    for line in new_list:
+        data.writelines(line)
+    data.close()    
+
 def show_list():
     data = open('file.txt', 'r', encoding='utf-8')
     num = 1
